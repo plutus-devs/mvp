5f8fa2d79b6ccc29f46c4f6eb608884ebc865b29
Nonthapat Kaewamporn <tontan2545@gmail.com>
2022-09-13 00:00:36 +0700

    add variable to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,19 @@
 python plutus/manage.py collectstatic --no-input
 
+if [ ! -f .env ]
+then
+  export "$(grep -v '^#' .env | xargs)"
+  echo "Injected env file"
+fi
+
 DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python plutus/manage.py createsuperuser --username "$SUPER_USER_NAME" --email "$SUPER_USER_EMAIL" --noinput
 
 if [ "$ENVIRONMENT" = "production" ]; then
-    ./wait-for.sh db:3306 -- make run-prod
+    echo "Waiting for host $MYSQL_HOST:$MYSQL_PORT"
+    ./wait-for.sh "$MYSQL_HOST":"$MYSQL_PORT" -- make run-prod
 elif [ "$ENVIRONMENT" = "development" ]; then
-    ./wait-for.sh db:3306 -- make run-dev
+    echo "Waiting for host $MYSQL_HOST:$MYSQL_PORT"
+    ./wait-for.sh "$MYSQL_HOST":"$MYSQL_PORT" -- make run-dev
 else
     echo "Invalid environment"
     exit 1
